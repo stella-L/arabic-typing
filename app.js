c1077ba4f21ff1de0d10627a83752a9da6ebbf84
Stella <stella@Mac.local>
2026-07-17 10:59:49 +0900

    Refresh data cache version

--- app.js
+++ app.js
@@ -392,7 +392,7 @@ allWords = FALLBACK;
 buildCategoryScreen();
 updateFavoriteCount();
 
-fetch('words.json?v=20260704-2')
+fetch('words.json?v=20260717-data')
   .then(r => r.json())
   .then(data => {
     allWords = data;
